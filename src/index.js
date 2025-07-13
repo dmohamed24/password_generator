f13ad70f6e525e682e588baeebf36e5e0b77c6e5
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2025-07-13 18:13:02 +0100

    GP-1: Fixed prettier formatting to test ci.yml workflow

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-      app.use(express.json());
-      app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('public'));
 
-      app.post('/generate', (req, res) => {
-        const { account , test } = req.body;
-    const password = crypto.randomBytes(12).toString('hex');
-        res.json({ account, password });
-      });
+app.post('/generate', (req, res) => {
+  const { account, test } = req.body;
+  const password = crypto.randomBytes(12).toString('hex');
+  res.json({ account, password });
+});
 
-      app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
-      });
+});
 
-      const server = app.listen(PORT, () => {
-            console.log(`App listening at http://localhost:${PORT}`);
-      });
+const server = app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
 
-      module.exports = { app, server };
+module.exports = { app, server };
